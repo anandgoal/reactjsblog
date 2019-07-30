402e10ec82cfea62e888aa143a3cebbd04a127e0
anandgoal <rahulviamail@gmail.com>
2019-07-30 15:20:59 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+  <div>
+    <h1>About our pages</h1>
+  </div>
+  </Layout>
+);
+
+export default About;
+
+
---
+++ pages/copy.js
@@ -0,0 +1,12 @@
+import Navbar from '../Components/Navbar';
+
+const Index = () => (
+  <div>
+    <Navbar/>
+    <h1>Welcome to Bitzprice</h1>
+  </div>
+);
+
+export default Index;
+
+
